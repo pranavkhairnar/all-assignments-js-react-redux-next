c2f0ede1e2692f7ee76464598ac60c6518b3565e
Pranav Khairnar <pranav.khairnar@mindstix.com>
2022-01-20 22:33:45 +0530

    react assignment 2 TODO app

---
+++ assignment12_todo_app/src/components/App.jsx
@@ -0,0 +1,73 @@
+import React, { useState } from "react";
+import Todo from "./Todo";
+
+function App() {
+  const [inputText, setInputText] = useState("");
+  const [items, setItems] = useState([]);
+
+  function handleChange(event) {
+    const newValue = event.target.value;
+    setInputText(newValue);
+  }
+
+  function addItem() {
+    setItems(prevItems => {
+      return [...prevItems, inputText];
+    });
+    setInputText("");
+  }
+
+  function deleteItem(id) {
+    //console.log(id);
+    setItems((prevItems) => {
+      return prevItems.filter(
+        (item, index) => {
+          return index !== id;
+        }
+      )
+    });
+  }
+  function editItem(id) {
+    //console.log(id);
+    
+    /*
+    setItems((prevItems) => {
+      return prevItems.filter(
+        (item, index) => {
+          return index !== id;
+        }
+      )
+    });
+    */
+    
+  }
+
+  return (
+    <div className="container">
+      <div className="heading">
+        <h1>To-Do List</h1>
+      </div>
+      <div className="form">
+        <input onChange={handleChange} type="text" value={inputText} />
+        <button onClick={addItem}>
+          <span>Add</span>
+        </button>
+      </div>
+      <div>
+        <ol>
+          {items.map((todoItem, index) => (
+            <Todo 
+            key = {index}
+            id = {index}
+            text={todoItem}
+              onChecked={deleteItem}
+              editChecked={editItem}
+            />
+          ))}
+        </ol>
+      </div>
+    </div>
+  );
+}
+
+export default App;
