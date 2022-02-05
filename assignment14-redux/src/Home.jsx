ce6225788a0370bccfd387fb6c21d79408ac23ee
Pranav Khairnar <pranav.khairnar@mindstix.com>
2022-02-05 16:22:22 +0530

    redux assignment final commit

--- assignment14-redux/src/Home.jsx
+++ assignment14-redux/src/Home.jsx
@@ -1,46 +1,47 @@
 import React from "react";
-import { useDispatch } from 'react-redux';
-import { bindActionCreators } from 'redux';
-import { actionCreators } from './state/index';
+import { useDispatch } from "react-redux";
+import { bindActionCreators } from "redux";
+import { actionCreators } from "./state/index";
+import { useSelector } from "react-redux";
 
-function Home(props) {
-  let items = props.items;
+function Home() {
   const dispatch = useDispatch();
-  const actions = bindActionCreators(actionCreators, dispatch); 
+  const actions = bindActionCreators(actionCreators, dispatch);
+  const allItems = useSelector((state) => state.allItems);
 
   return (
     <>
-  <br></br>
-  <br></br>
-  <br></br>
-  <div className="mx-auto row row-cols-1 row-cols-md-6" style={{width: "95%"}}>
-      {items.map((item) => (
-    
-    
-    <div className="card" > 
-    <img src={item.imgurl} className="card-img-top" alt="hi"></img>
-    <div className="card-body">
-    <h5 className="card-title">{item.item}</h5>
-    <p className="card-text">Price: ₹{item.price}</p>
-    
-    <button onClick={() => {actions.withdrawMoney(100)}} className="btn btn-dark">Add to cart</button>
+      <br></br>
+      <br></br>
+      <br></br>
+      <div
+        className="mx-auto row row-cols-1 row-cols-md-6"
+        style={{ width: "95%" }}
+      >
+        {allItems.map((item) => (
+          <div className="card">
+            <img src={item.imgurl} className="card-img-top" alt="hi"></img>
+            <div className="card-body">
+              <h5 className="card-title">{item.item}</h5>
+              <p className="card-text">Price: ₹{item.price}</p>
+
+              <button
+                onClick={() => {
+                  actions.addToCart(item.id);
+                }}
+                className="btn btn-dark"
+              >
+                Add to cart
+              </button>
+            </div>
           </div>
-    {/*
-      <button onClick={() => props.addToCart(item.id)} className="btn btn-dark">Add to cart</button>
-          </div>
-    */}
-    
-          
-    
-    </div>
-      ))}
-  </div>
-  <br></br>
-  <br></br>
-  <br></br>
+        ))}
+      </div>
+      <br></br>
+      <br></br>
+      <br></br>
     </>
   );
 }
-
 
 export default Home;
