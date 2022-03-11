a9d4571547934f6af34baa851f7926859eb52f83
Pranav Khairnar <pranav.khairnar@mindstix.com>
2022-03-11 15:57:53 +0530

    axios in next-api folder

--- next-api/pages/_app.js
+++ next-api/pages/_app.js
@@ -1,6 +1,19 @@
 import '../styles/globals.css'
 
+import axios from "axios";
+
+const api = axios.create({
+  baseURL: `http://localhost:3000/api/comments`
+})
+
 function MyApp({ Component, pageProps }) {
+
+  
+    api.get('/').then(res => {
+      console.log(res.data);
+    })
+
+
   return <Component {...pageProps} />
 }
 
